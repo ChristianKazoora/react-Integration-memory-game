3494e98a7818844886b3029ed3442b1b84dde087
ChristianKazoora <christianmkazooora@yahoo.com>
2024-05-20 00:52:44 -0300

    formating

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Cards from "./components/Cards";
 import "./App.css";
 
 function App() {
@@ -9,6 +8,30 @@ function App() {
   return (
     <>
       <h1 className="btn">Hello world!</h1>
+      <div className="App">
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "center",
+            padding: "10px",
+          }}
+        >
+          <h1
+            style={{
+              display: "flex",
+              backgroundColor: "lavender",
+              width: "max-content",
+              border: "2px solid purple",
+              borderRadius: "5px",
+              padding: "3px",
+            }}
+          >
+            Integration Flashcards
+          </h1>
+        </div>
+        <Cards />
+      </div>
     </>
   );
 }
